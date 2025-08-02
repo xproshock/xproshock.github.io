ae2ae9f8050a1ef3a13334a83c62050ebee7c768
Shockfr <xpro2058@gmail.com>
2025-08-01 18:53:38 -0700

    Test

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_input = request.form['user_input']
+    # You can process the input here, for example, save to a file or database
+    with open("output.txt", "w") as f:
+        f.write(user_input)
+    return f'You entered: {user_input}. The input has been saved to output.txt.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
